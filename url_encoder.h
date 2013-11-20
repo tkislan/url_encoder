13ae8e6b9d39a95b7d6c8543b8672fe8d3b0e5a3
Tomas Kislan <kislan.tomas@gmail.com>
2013-11-20 13:14:13 +0100

    Create url_encoder.h

---
+++ url_encoder.h
@@ -0,0 +1,108 @@
+#ifndef URL_ENCODER_H
+#define URL_ENCODER_H
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+const char kUnreservedChar[] = {
+//0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 0
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 1
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, // 2
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, // 3
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 4
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, // 5
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // 6
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, // 7
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 8
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 9
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // A
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // B
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // C
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // D
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // E
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0  // F
+};
+
+const char kHexToNum[] = {
+// 0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 1
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 2
+   0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1, // 3
+  -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 4
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 5
+  -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 6
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 7
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 8
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 9
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // A
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // B
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // C
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // D
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // E
+  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  // F
+};
+
+class UrlEncoder {
+ public:
+  static inline void Encode(const std::string &in, std::string *out) {
+    std::stringstream ss;
+
+    for (std::string::const_iterator it = in.begin(); it != in.end(); ++it) {
+      if (kUnreservedChar[*it]) ss << *it;
+      else ss << '%' << std::setfill('0') << std::hex << (int)*it;
+    }
+
+    out->assign(ss.str());
+  }
+
+  static inline std::string Encode(const std::string &in) {
+    std::string out;
+    Encode(in, &out);
+    return out;
+  }
+
+  static inline bool Decode(const std::string &in, std::string *out) {
+    std::stringstream ss;
+
+    std::string::const_iterator it = in.begin();
+
+    char tmp, encoded_char;
+    while (it != in.end()) {
+      if (*it == '%') {
+        if (++it == in.end()) return false;
+        tmp = kHexToNum[*it];
+        if (tmp < 0) return false;
+        encoded_char = (16 * tmp);
+
+        if (++it == in.end()) return false;
+        tmp = kHexToNum[*it];
+        if (tmp < 0) return false;
+        encoded_char += tmp;
+
+        ++it;
+
+        ss << encoded_char;
+      } else {
+        ss << *it;
+        ++it;
+      }
+    }
+
+    out->assign(ss.str());
+
+    return true;
+  }
+
+  static inline std::string Decode(const std::string &in) {
+    std::string out;
+    Decode(in, &out);
+    return out;
+  }
+
+ private:
+};
+
+#endif // URL_ENCODER_H
